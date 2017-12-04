b504802a063fc5a623c55dd2438d33d93fc6e448
Jakub Valenta <33723739+jakub-valenta@users.noreply.github.com>
2017-12-04 16:56:14 +0100

    Puzzle 4 second star

--- src/aoc4.rs
+++ src/aoc4.rs
@@ -1,4 +1,5 @@
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 pub fn verify_pass_phrase(pass_phrase: &str) -> bool {
     let mut words = HashSet::new();
@@ -11,9 +12,32 @@ pub fn verify_pass_phrase(pass_phrase: &str) -> bool {
     true
 }
 
+pub fn verify_pass_phrase_anagram(pass_phrase: &str) -> bool {
+    let mut words = HashSet::new();
+    for word in pass_phrase.split(' ') {
+        let mut word = word.chars().collect::<Vec<char>>();
+        word.sort();
+        let word = String::from_iter(word);
+        if words.contains(&word) {
+            return false;
+        }
+        words.insert(word);
+    }
+    true
+}
+
 #[test]
 fn test_examples() {
     assert!(verify_pass_phrase("aa bb cc dd ee"));
     assert!(!verify_pass_phrase("aa bb cc dd aa"));
     assert!(verify_pass_phrase("aa bb cc dd aaa"));
+}
+
+#[test]
+fn test_examples_anagram() {
+    assert!(verify_pass_phrase_anagram("abcde fghij"));
+    assert!(!verify_pass_phrase_anagram("abcde xyz ecdab"));
+    assert!(verify_pass_phrase_anagram("a ab abc abd abf abj"));
+    assert!(verify_pass_phrase_anagram("iiii oiii ooii oooi oooo"));
+    assert!(!verify_pass_phrase_anagram("oiii ioii iioi iiio"));
 }
--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,44 @@ mod aoc4;
 
 use std::env;
 use std::error::Error;
-use std::num::ParseIntError;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
+    if args.len() < 3 {
         println!("Invalid arguments!");
         return;
     }
-    let puzzle = args[1].parse::<u32>();
+    let puzzle = args[1].parse::<u32>().unwrap_or(100);
+    let star = args[2].parse::<u32>().unwrap_or(100);
+    if star > 2 || puzzle > 25 {
+        println!("Invalid arguments!");
+        return;
+    }
     match puzzle {
-        Ok(1) => {
-            let star = args[2].parse::<u32>();
-            let captcha = match star {
-                Ok(1) => aoc1::sum_digits(&args[3]),
-                Ok(2) => aoc1::sum_digits_half(&args[3]),
-                _ => None,
+        1 => {
+            let captcha = if star == 1 {
+                aoc1::sum_digits(&args[3])
+            } else {
+                aoc1::sum_digits_half(&args[3])
             };
             match captcha {
                 Some(x) => println!("Captcha solved {}!", x),
                 None => println!("Invalid captcha input!"),
             };
         }
-        Ok(2) => {
-            let star = args[2].parse::<u32>();
-            let checksum = match star {
-                Ok(1) => aoc2::checksum(&merge_args(&args, 3, "\n")),
-                Ok(2) => aoc2::checksum_div(&merge_args(&args, 3, "\n")),
-                Ok(_) => aoc2::checksum("x"),
-                Err(e) => Err(e),
+        2 => {
+            let checksum = if star == 1 {
+                aoc2::checksum(&merge_args(&args, 3, "\n"))
+            } else {
+                aoc2::checksum_div(&merge_args(&args, 3, "\n"))
             };
             match checksum {
                 Ok(x) => println!("Checksum calculated {}!", x),
                 Err(e) => println!("Invalid checksum input {}!", e.description()),
             }
         }
-        Ok(3) => {
-            let cell = args[2].parse::<u32>();
+        3 => {
+            let cell = args[3].parse::<u32>();
             match cell {
                 Ok(cell) => {
                     let steps = aoc3::distance(cell);
@@ -53,15 +54,19 @@ fn main() {
                 Err(err) => println!("Invalid input {}!", err),
             }
         }
-        Ok(4) => {
-            if aoc4::verify_pass_phrase(&merge_args(&args, 2, " ")) {
-                println!("Passphrase correct!");
+        4 => {
+            let valid = if star == 1 {
+                aoc4::verify_pass_phrase(&merge_args(&args, 3, " "))
             } else {
-                println!("Passphrase incorrect!");
+                aoc4::verify_pass_phrase_anagram(&merge_args(&args, 3, " "))
+            };
+            if valid {
+                println!("Correct passphrase!");
+            } else {
+                println!("Incorrect passphrase!");
             }
         }
-        Ok(_) => println!("Unknown puzzle!"),
-        Err(err) => println!("Invalid input {}!", err),
+        _ => println!("Unknown puzzle!"),
     }
 }
 
